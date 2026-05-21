c83ec4b6643de2a7b52ceead64cd969060b0a963
Alsanad-Opium <alsanad112006@gmail.com>
2026-05-21 17:31:24 +0530

    Created the model is books

---
+++ app/models.py
@@ -0,0 +1,25 @@
+from app import db
+from datetime import datetime, timezone
+class Books(db.Model):
+    
+    __tablename__ = 'books'
+    
+    id = db.Column(db.Integer, primary_key =True)
+    title =db.Column(db.String(100), nullable = False)
+    author =db.Column(db.String(100), nullable = False)
+    genre = db.Column(db.String(50), nullable = False)
+    published_date = db.Column(db.DateTime, nullable = False, default = datetime.now(timezone.utc))
+    
+    
+    def to_dict(self):
+        return{
+            "id": self.id,
+            "title": self.title,
+            "author": self.author,
+            "genre": self.genre,
+            "published_date": self.published_date
+        }
+    
+    
+    def __repr__(self):
+        return f" Book = {self.title}, Author = {self.author}, Genre = {self.genre}, Published Date = {self.published_date}"
