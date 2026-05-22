0117dffd55acb9ca2746a8f749e5aa33bc9866d5
Alsanad-Opium <alsanad112006@gmail.com>
2026-05-22 12:43:04 +0530

    commands

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ app = create_app()
 
 if __name__ == ' __main__':
     app.run(debug = True)
+    
+    # cmd to run Once the app is created and the models are defined we need to create the database and the tables in the databse using the flask-migrate package which is a wrapper around alembic to handle database migrations in flask applications
+    
+#     flask --app app db init
+# flask --app app db migrate -m "create books table"
+# flask --app app db upgrade
