1d498c0454c69c8c2273e36cd75139503b699e21
Alsanad-Opium <alsanad112006@gmail.com>
2026-05-22 17:03:39 +0530

    Adding JWT login and protected paths for api_endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,25 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from flask_bcrypt import bcrypt
 from dotenv import load_dotenv
 
 
 
 load_dotenv() ## load the var from .env
+
 db =SQLAlchemy() # create db instance at module level so it can be imported in other modules
-migrate = Migrate() # create migrate module at modeul level so can be used by other modules  
+
+migrate = Migrate() 
+# create migrate module at modeul level so can be used by other modules  
 #it is used to handle database migration that is handle the changing of the database schema like the add columns rename columns and delete columns without losing the data in the database 
 
+jwt = JWTManager() 
+#object that is used to handle the jwt authentication in the app it is used to create and verify the jwt tokens and also to handle the jwt related errors
+
+bcrypt = bcrypt() 
+# object that is used to handle the password hashing and checking in the app it is used to hash the password before storing it in the database and also to check the password when the user login
 
 def create_app():
     
@@ -18,7 +28,8 @@ def create_app():
     
     db.init_app(app) # initialize the db with the app can be used by other modules to interact with the database 
     migrate.init_app(app,db) # same as above just for migration 
-    
+    jwt.init_app(app)
+    bcrypt.init_app(app)
     from app.routes.books import books_bp
     from app import models# import the blueprint from the routes module to register it with the app
     app.register_blueprint(books_bp) # register the blueprint with the app to make the routes defined in the blueprint available to the app
--- config.py
+++ config.py
@@ -7,5 +7,6 @@ dotenv.load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     
     
