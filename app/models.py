10a6d880d205410e6ba4c71b2529f5f5fb6ed692
Alsanad-Opium <alsanad112006@gmail.com>
2026-05-22 23:15:58 +0530

    Users table schema

--- app/models.py
+++ app/models.py
@@ -23,3 +23,25 @@ class Books(db.Model):
     
     def __repr__(self):
         return f" Book = {self.title}, Author = {self.author}, Genre = {self.genre}, Published Date = {self.published_date}"
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(100),nullable  = False, unique = True)
+    email = db.Column(db.String(100),nullable  = False, unique = True)
+    password = db.Column(db.String(225), nullable = False)
+    created_at = db.Column(db.DateTime, nullable = False, default = datetime.now(timezone.utc))
+    
+    
+   
+    def to_dict(self):
+        return{
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "created_at": self.created_at
+        }
+        
+    def __repr__(self):
+        return f"User ={self.username}, Email = {self.email}, Created At = {self.created_at}"
