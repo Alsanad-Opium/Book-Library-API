4dc6e6cb83cc04f372f6c417400c9ef5e4e446ae
Alsanad-Opium <alsanad112006@gmail.com>
2026-05-23 12:29:24 +0530

    added the register method

---
+++ app/routes/auth.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import create_access_token
+from app.models import User
+from app import db, bcrypt
+
+
+
+auth_bp = Blueprint('auth',__name__, url_prefix = "/api/auth")
+
+@auth_bp.route('/register',methods = ["POST"])
+
+def register():
+    data = request.get_json()
+    
+    if not data.get('username') or not data.get('email') or not data.get('password'):
+        return jsonify({"message": "Username email and password are required fields"}),400
+    
+    existing_email = User.query.filter_by(email = data['email']).first()
+    existing_username = User.query.filter_by(username = data['username']).first()
+    
+    if existing_email or existing_username:
+        return jsonify({"message": "Email or username already exists"}),400
+    
+    hashed_password =  bcrypt.generate_password_hash(data['password']).decode('utf-8')
+    user = User(
+        username = data['username'],
+        email = data['email'],
+        password = hashed_password
+    )
+    
+    db.session.add(user)
+    db.session.commit()
+    
+    return jsonify({"message": "USer added successfully ", "user": user.to_dict()}),201
+    
