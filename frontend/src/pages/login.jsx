920ab2cb840a61d9b655ca11adcbcccc82f67ff1
Alsanad-Opium <alsanad112006@gmail.com>
2026-05-25 19:39:49 +0530

    added the login page in the app with error handling and redirecting to the api endpoint /books

---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,60 @@
+import {userState}  from 'react';
+import api from '../api/axios';
+
+
+export default function Login(){
+
+    const [username, setUsername] = useState('');
+    const [password,setPassword] = useState('');
+    const [error,setError] = useState('');
+
+
+    async function  handleSubmit(e){
+        e.preventDefault();// prevent the default form submission behavior, which would cause a page reload.
+        
+       try{
+
+         const response = await api.post('/auth/login', {username,password});
+
+
+        localStorage.setItem('token',response.data.access_token); // store the JWT token in localStorage. This allows us to persist the user's login state across page refreshes and browser sessions.
+        
+        localStorage.setItem('user',response.data.user);
+
+        window.location.href('/books'); // redirect to the books page after successful login. This is a simple way to navigate, but in a real app, you'd likely use React Router for better navigation handling.
+       } 
+       catch(error){
+        setError(error.response?.data?.meassage || "Login failed "); // set an error message if the login fails. This will display feedback to the user if their credentials are incorrect or if there's a server issue.
+       }
+
+        
+    }
+
+    return(
+         <div>
+      <h2>Login</h2>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>Login</button>
+
+      <p>
+        Don't have an account? <a href="/register">Register</a>
+      </p>
+    </div>
+    );
+}
