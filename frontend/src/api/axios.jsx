d630e13f6191feda574c0c86c7368f39c192b18f
Alsanad-Opium <alsanad112006@gmail.com>
2026-05-25 15:41:01 +0530

    Create the baseURL and use token for every request

---
+++ frontend/src/api/axios.jsx
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL :"http://127.0.0.1:5000/api" // this is the base URL for all our API requests. It points to our Flask backend.
+
+});
+
+
+axios.interceptors.request.use((config)=>{
+// an interceptor runs automatically before every request you make. Think of it as a middleware for your frontend. Industry standard pattern for handling auth tokens.
+    
+    const token = localStorage.getItem('token');
+
+    if (token){
+        config.headers.Authorization = `Bearer ${token}`; //config.headers.Authorization = \Bearer ${token}`** — attaches the token to every request automatically. This is why @jwt_required()` works on the backend — it looks for this exact header format.
+    return config ;
+    }
+});
+// you must return the config or the request won't go through.
+
+export default api
